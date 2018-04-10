e9b92ebefa1e8a5a5ffb5068fccb2d8454176022
alexrivadeneira <alexrivs@gmail.com>
2018-04-10 16:36:18 -0700

    zuul test

--- api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
@@ -2,6 +2,7 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
