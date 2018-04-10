4bb1400f98d87f2e667be0e1a4fd5bfddd5ae50d
alexrivadeneira <alexrivs@gmail.com>
2018-04-10 16:31:07 -0700

    imports

--- api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
@@ -2,7 +2,11 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
