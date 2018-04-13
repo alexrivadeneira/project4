43845eb7dba68041a9cc001a93960a9db99adc56
alexrivadeneira <alexrivs@gmail.com>
2018-04-13 11:16:17 -0700

    ui feature test dummy users

--- users-api/src/test/java/com/example/usersapi/features/UsersUIFeatureTest.java
+++ users-api/src/test/java/com/example/usersapi/features/UsersUIFeatureTest.java
@@ -1,12 +1,16 @@
 package com.example.usersapi.features;
 
+import com.example.usersapi.models.User;
 import com.example.usersapi.repositories.UserRepository;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Stream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -23,5 +27,27 @@ public class UsersUIFeatureTest {
     @After
     public void tearDown() {
         userRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldAllowFullCrudFunctionalityForAUser() throws Exception {
+
+        User firstUser = new User(
+                "someone",
+                "Ima",
+                "Person"
+        );
+
+        User secondUser = new User(
+                "someone_else",
+                "Someone",
+                "Else"
+        );
+
+        Stream.of(firstUser, secondUser)
+                .forEach(user -> {
+                    userRepository.save(user);
+                });
+
     }
 }
